0dd269d871c7fa4cabe6facc706671d6946656da
merouanezouaid <marwan.zouaid@gmail.com>
2022-04-29 22:03:01 +0000

    Updates

---
+++ precache-manifest.8f99199d82401d7167ed102ebd148572.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e12f7fc4d909ee9cef327f07563c2616",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15f3a625b0e54f75f21a",
+    "url": "/static/css/2.62260641.chunk.css"
+  },
+  {
+    "revision": "4a8960ccb0532803c7c2",
+    "url": "/static/css/main.57011498.chunk.css"
+  },
+  {
+    "revision": "15f3a625b0e54f75f21a",
+    "url": "/static/js/2.b16719e2.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.b16719e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8960ccb0532803c7c2",
+    "url": "/static/js/main.97ecd9ff.chunk.js"
+  },
+  {
+    "revision": "e377ba1360f48ce7c076",
+    "url": "/static/js/runtime-main.c2ecb867.js"
+  },
+  {
+    "revision": "a061ebc3e94930a114b3beee1b3cc6a2",
+    "url": "/static/media/mypic.a061ebc3.jpg"
+  }
+]);
